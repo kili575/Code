cff51864634737829fa746b958dc95a0a9d473fa
Kilian Ertl <ertlkilian22@gmail.com>
2026-04-23 15:24:11 +0200

    merge_sort fuction implementation

--- merge_sort.py
+++ merge_sort.py
@@ -3,6 +3,22 @@ import numpy as np
 unsorted_numbers = np.random.randint(1, 10, 10)
 
 def merge_sort(array):
+    if len(array) <= 1:
+        return array
     
+    middle = len(array) // 2
+    left_array = np.empty(middle, dtype=int)
+    right_array = np.empty(len(array) - middle, dtype=int)
 
-def sort(left_array, right_array, array):
+    for i in range(len(array)):
+        if(i < middle):
+            left_array[i] = array[i]
+        else:
+            right_array[i - middle] = array[i] 
+    
+    merge_sort(left_array)
+    merge_sort(right_array)
+    merge(left_array, right_array, array)
+
+def merge(left_array, right_array, array):
+    
